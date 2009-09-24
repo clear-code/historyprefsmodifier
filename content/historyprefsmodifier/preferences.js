e6e897b671fbe61a426e56c05ca04ebb9eef11a9
SHIMODA Hiroshi <shimoda@clear-code.com>
2009-09-24 15:00:10 +0000

    UIが切り替わっていなかった

--- content/historyprefsmodifier/preferences.js
+++ content/historyprefsmodifier/preferences.js
@@ -2,25 +2,29 @@ window.addEventListener('DOMContentLoaded', function() {
 	window.removeEventListener('DOMContentLoaded', arguments.callee, false);
 
 	window.addEventListener('paneload', function() {
-		if (!('gPrivacyPane' in window)) return;
-
 		var menulist = document.getElementById('historyMode');
-		menulist.value = 'custom';
-
-		var historyDays = document.getElementById('historyDays');
-		historyDays.setAttribute('onsynctopreference', <![CDATA[
-			var historyDays = document.getElementById('historyDays');
-			var value = parseInt(maxPref.defaultValue);
-			var maxPref = document.getElementById('browser.history_expire_days');
-			var minPref = document.getElementById('browser.history_expire_days_min');
-			if (maxPref.defaultValue == minPref.defaultValue ||
-				parseInt(maxPref.value) < value) {
-				maxPref.value = value;
-			}
-			return value;
-		]]>);
+		if (!menulist) return;
 
 		window.removeEventListener('paneload', arguments.callee, false);
+
+		window.setTimeout(function() {
+			menulist.value = 'custom';
+			gPrivacyPane.updateHistoryModePane();
+			gPrivacyPane.updatePrivacyMicroControls();
+
+			var historyDays = document.getElementById('historyDays');
+			historyDays.setAttribute('onsynctopreference', <![CDATA[
+				var historyDays = document.getElementById('historyDays');
+				var value = parseInt(maxPref.defaultValue);
+				var maxPref = document.getElementById('browser.history_expire_days');
+				var minPref = document.getElementById('browser.history_expire_days_min');
+				if (maxPref.defaultValue == minPref.defaultValue ||
+					parseInt(maxPref.value) < value) {
+					maxPref.value = value;
+				}
+				return value;
+			]]>);
+		}, 0);
 	}, false);
 
 }, false);
