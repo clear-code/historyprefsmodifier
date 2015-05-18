f530ad161b99dc1eeb99011e9d46135ea402705b
YUKI Hiroshi <shimoda@clear-code.com>
2015-05-18 15:45:34 +0900

    Fix typo: missing "]"

--- modules/updateUI.js
+++ modules/updateUI.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-var EXPORTED_SYMBOLS = ['historyPrefsModifierUpdateUI';
+var EXPORTED_SYMBOLS = ['historyPrefsModifierUpdateUI'];
 
 function historyPrefsModifierUpdateUI(aWindow) {
   var menulist = aWindow.document.getElementById('historyMode');
